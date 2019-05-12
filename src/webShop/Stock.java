66363e63b44100eba0afb000b3261a8dc56b4c83
Ohrling <ohrling@users.noreply.github.com>
2019-05-12 16:18:07 +0200

    Minor changes in Stock.java

--- src/webShop/Stock.java
+++ src/webShop/Stock.java
@@ -1,11 +1,22 @@
 package webShop;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stock {
-	private List<Article> stockList;
+	private StockImport si;
+	private List<Article> stockList = new ArrayList<>();
+
+	// Alternative contructors/Different parameters in the future for separate import?
 	public Stock() {
-		// TODO Auto-generated constructor stub
+		si = new SmallCouchImport();
+		stockList.addAll(si.getStock());
+		si = new BigCouchImport();
+		stockList.addAll(si.getStock());
+		si = new SmallTableImport();
+		stockList.addAll(si.getStock());
+		si = new BigTableImport();
+		stockList.addAll(si.getStock());
 	}
 	
 	/**
